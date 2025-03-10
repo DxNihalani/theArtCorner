e405b7f277a23aa83290ef77f4fde421280f74da
mansi-k-555 <mkortikar@deloitte.com>
2025-03-10 15:54:37 +0530

    changes

--- src/app/authservice.service.ts
+++ src/app/authservice.service.ts
@@ -5,15 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   private loggedInUser: any = null;
+  private storedUser: any = null;
 
   constructor() { }
 
   setLoggedInUser(user: any) {
     this.loggedInUser = user;
+    this.storedUser = user; // Store the user information
   }
 
   getLoggedInUser() {
     return this.loggedInUser;
+  }
+
+  getStoredUser() {
+    return this.storedUser;
   }
 
   isLoggedIn() {
