b3d479c41efc0b13bf93d361a07e22e0377ec87b
mansi-k-555 <mkortikar@deloitte.com>
2025-03-10 12:15:55 +0530

    changes

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -17,15 +17,21 @@ export class RegistrationComponent {
   };
 
   isRegistered = false; // Add this flag
+  errorMessage = ''; // Add this flag
 
   constructor(private userService: UserService) { }
 
-  onSubmit() {
-    this.userService.addUser(this.user).subscribe(response => {
-      console.log('User added successfully', response);
-      this.isRegistered = true; // Set the flag to true on successful registration
-      alert('You are registered successfully!'); // Show alert message
-      window.open('/login', '_blank'); // Open login page in a new tab
-    });
+  onSubmit(registrationForm: any) {
+    if (registrationForm.valid) {
+      this.userService.addUser(this.user).subscribe(response => {
+        this.isRegistered = true; // Set the flag to true on successful registration
+        this.errorMessage = ''; // Clear the error message
+        alert('You are registered successfully!'); // Show alert message
+        window.open('/login', '_blank'); // Open login page in a new tab
+      });
+    } else {
+      this.isRegistered = false; // Ensure the success message is not shown
+      this.errorMessage = 'Please enter all valid details.';
+    }
   }
 }
