7bf8ede00a5f73f14d7603e373b8820e095af69c
DxNihalani <dnihalani@deloitte.com>
2025-03-10 16:29:45 +0530

    Cart Complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
-  { path: '', redirectTo: '/registration', pathMatch: 'full' } ,
-  {path:'cart', component:CartComponent}]
+  //{ path: '', redirectTo: '/registration', pathMatch: 'full' } ,
+  {path:'cart', component:CartComponent},
+  {path:'product', component:ProductsComponent}
+  
+]
   // Default route
 
 
