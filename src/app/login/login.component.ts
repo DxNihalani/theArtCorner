38a3359adc96d41b9102aa6329d7bad9f617fb51
mansi-k-555 <mkortikar@deloitte.com>
2025-03-10 13:52:13 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../authservice.service';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +12,20 @@ export class LoginComponent {
   email = '';
   pwd = '';
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   onSubmit() {
     this.userService.getUsers().subscribe(users => {
       const user = users.find((u: any) => u.email === this.email && u.pwd === this.pwd);
       if (user) {
+        this.authService.setLoggedInUser(user);
         alert('Login successful');
         // Redirect to the desired page
+        this.router.navigate(['/dashboard']); // Replace with your desired path
       } else {
         alert('Invalid credentials');
       }
