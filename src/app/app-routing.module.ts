f53c339314130b49d53aac8bb4cf3913fcaf00fe
mansi-k-555 <mkortikar@deloitte.com>
2025-03-10 14:02:50 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: '', redirectTo: '/registration', pathMatch: 'full' } ,
+  { path: 'dashboard', component: DashboardComponent},
   {path:'cart', component:CartComponent}]
   // Default route
 
